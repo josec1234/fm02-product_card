5e86a7b6f8782d83011fb29ec9bf0330a23eda57
Jose Carlos <josecralosm@gmail.com>
2023-08-05 09:38:22 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+function App() {
+  return (
+    <main>
+      Perfume
+
+        Gabrielle Essence Eau De Parfum
+
+        A floral, solar and voluptuous interpretation composed by Olivier Polge, 
+        Perfumer-Creator for the House of CHANEL.
+
+        $149.99
+        $169.99
+
+        Add to Cart
+      <footer className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
+      </footer>
+    </main>
+  )
+}
+
+export default App
